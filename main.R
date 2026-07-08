58ee618d8d3610e94ead4ba1e5d3699a64a39d52
Sully <sharrer619@gmail.com>
2026-07-08 19:26:02 -0400

    Outline new models + new plots

--- main.R
+++ main.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(ggplot2)
 library(lme4)
 library(performance)
+library(MuMIn)
+library(glmmTMB)
 
 trial_data <- read.csv("trial_data.csv")
 collection_data <- read.csv("collection_data.csv")
@@ -41,7 +43,8 @@ trial_data <- trial_data %>%
     starvation_duration_mins = as.numeric(difftime(trial_start, 
                                                    starving_start, 
                                                    units = "mins")),
-    chose_trt = first_choice == trt_arm,
+    chose_trt = as.numeric(first_choice == trt_arm),
+    
   )
 trial_data <- trial_data %>% 
   mutate(
@@ -91,13 +94,14 @@ trial_data <- trial_data %>%
   arrange(trial_start) %>%
   tibble::rowid_to_column("trial_ID") %>%
   mutate(
-    z_starvation_duration_mins = as.numeric(scale(starvation_duration_mins)),
-    z_temp = as.numeric(scale(temp)),
-    z_pct_sodium_error = as.numeric(scale(pct_sodium_error)),
-    z_time_since_sodium_added = as.numeric(scale(time_since_sodium_added)),
-    z_time_of_day_hrs = as.numeric(scale(time_of_day_hrs)),
-    prop_trt_time_secs = trt_time_secs / TRIAL_DUR_SECS,
-    prop_ctrl_time_secs = ctrl_time_secs / TRIAL_DUR_SECS
+    starvation_duration_mins_z = as.numeric(scale(starvation_duration_mins)),
+    temp_z = as.numeric(scale(temp)),
+    pct_sodium_error_z = as.numeric(scale(pct_sodium_error)),
+    time_since_sodium_added_z = as.numeric(scale(time_since_sodium_added)),
+    time_of_day_hrs_z = as.numeric(scale(time_of_day_hrs)),
+    prop_trt_time_secs = trt_time_secs / (trt_time_secs + ctrl_time_secs),
+    # adjusted for beta regression
+    adj_prop_trt_time_secs = (prop_trt_time_secs * (nrow(trial_data) - 1) + 0.5)/nrow(trial_data)
   )
 
 nrow(trial_data) # should match pre-join row count
@@ -118,105 +122,78 @@ time_in_arm_wilcox_test <- wilcox.test(trial_data$trt_time_secs,
                                        paired = TRUE
                                        )
 
-
-
 # ============================== models =====================================
 
-preference_model <- glmer(
-  chose_trt ~ trt_arm + start_date + z_time_of_day_hrs + 
+pref_glmm.full <- glmer(
+  chose_trt ~ trt_arm + start_date + time_of_day_hrs_z + 
     (1 | batch_id) + (1 | location),  
-  data = trial_data, family = binomial, na.action = "na.fail"
+  data = trial_data, 
+  family = binomial, 
+  na.action = "na.fail"
 )
-summary(preference_model)
+summary(pref_glmm.full)
+check_collinearity(pref_glmm.full)
 
-library(car)
+# dharma of full model here
 
-Anova(preference_model, type = "II")
+pref_glmm.best <- get.models(dredge(pref_glmm.full), 1)[[1]]
+summary(pref_glmm.best)
+
+# dharma of best model here
 
 
-check_collinearity(preference_model)
-check_model(preference_model)
-
-library(MuMIn)
-dredge(preference_model)
-preference_model.dredge <- glmer(
-  chose_trt ~ 1 + 
+time_glmm.full <- glmmTMB(
+  adj_prop_trt_time_secs ~ trt_arm + start_date + time_of_day_hrs_z + 
     (1 | batch_id) + (1 | location),  
-  data = trial_data, family = binomial, na.action = "na.fail"
+  data = trial_data, 
+  family = beta_family(), 
+  na.action = "na.fail"
 )
-summary(preference_model.dredge)
-plogis(0.1478) # this is the back transformed preference
-check_model(preference_model.dredge)
+summary(time_glmm.full)
+check_collinearity(time_glmm.full)
 
-library(glmmTMB)
+# dharma of full model here
 
-trt_time_model <- glmmTMB(
-  prop_trt_time_secs ~ trt_arm + start_date + z_time_of_day_hrs + 
-    (1 | batch_id) + (1 | location),  
-  data = trial_data %>% filter(!(prop_trt_time_secs == 0)), family = beta_family(), na.action = "na.fail"
+time_glmm.best <- get.models(dredge(time_glmm.full), 1)[[1]]
+summary(pref_glmm.best)
+
+# dharma of best model here
+
+
+visits_glmm.full <- glmer(cbind(trt_visits, ctrl_visits) ~ trt_arm + 
+                            start_date + time_of_day_hrs_z + 
+                            (1 | batch_id) + (1 | location),  
+                          data = trial_data,
+                          family = binomial, 
+                          na.action = "na.fail"
 )
+summary(visits_glmm.full)
+check_collinearity(visits_glmm.full)
 
-summary(trt_time_model)
-check_collinearity(trt_time_model)
+# dharma of full model here
 
-lm <- glmer(cbind(trt_visits, ctrl_visits) ~ 1 + (1 | batch_id) + (1 | location),  
-      data = trial_data, family = "binomial")
-summary(lm)
+visits_glmm.best <- get.models(dredge(time_glmm.full), 1)[[1]]
+summary(visits_glmm.best)
+
+# dharma of best model here
 
 # =============================== plots =====================================
 
-plot_data <- data.frame(
-  proportion = preference_binomial_test$estimate,
-  lower = preference_binomial_test$conf.int[1],
-  upper = preference_binomial_test$conf.int[2]
-)
-
-ggplot(plot_data, aes(x = "Sodium arm", y = proportion)) +
-  geom_col() +
-  geom_errorbar(aes(ymin = lower, ymax = upper)) +
-  geom_hline(yintercept = 0.5, linetype = "dashed", color = "red") +
-  scale_y_continuous(limits = c(0, 1), labels = scales::percent) +
-  labs(
-    x = NULL,
-    y = "Proportion choosing sodium arm",
-    title = "First-choice preference for sodium-enriched arm"
-  )
-
-trial_data$chose_trt_binary <- as.numeric(trial_data$chose_trt)
-
-trial_data %>%
-  ggplot(aes(x = "", y = chose_trt_binary)) +
+ggplot(trial_data, aes(x = "", y = chose_trt)) +
   geom_jitter(height = 0) +
   stat_summary(fun.data = "mean_cl_boot")
 
-?ggplot()
-
-
-box_data <- data.frame(
-  duration = c(trial_data$trt_time_secs, trial_data$ctrl_time_secs),
-  visits = c(trial_data$trt_visits, trial_data$ctrl_visits),
-  is_trt = c(rep("Treatment", nrow(trial_data)), rep("Control", nrow(trial_data)))
-)
-
-ggplot(box_data, aes(x = is_trt, y = duration)) +
-  geom_boxplot() + 
-  labs(
-    x = "Y-maze arm",
-    y = "Duration of time spent in arm (secs)",
-    title = ""
+plot_data <- trial_data %>% 
+  select(trial_ID, trt_time_secs, ctrl_time_secs, trt_visits, ctrl_visits) %>% 
+  pivot_longer(
+    cols = -trial_ID,
+    names_to = c("arm", "metric"),
+    names_pattern = "^(trt|ctrl)_(.*)$",
+    values_to = "value"
   )
- ggplot(box_data, aes(x = is_trt, y = visits)) +
-  geom_boxplot() + 
-   labs(
-     x = "Y-maze arm",
-     y = "Number of visits",
-     title = ""
-   )
 
-
-# next: dredge using the dredge function then refit the model using the dredge results using: 
-  # 1. dredge results <- dredge(...)
-  # 2. best model <- get.models(dredge results, 1)[[1]]
-  # use saveRDS() on the good model
-
-# predict(model, type = "response") or the ggeffects/sjPlot package's ggpredict()
+ggplot(plot_data, aes(x = arm, y = value)) + 
+  geom_boxplot() +
+  geom_point() + 
+  geom_line(aes(group = trial_ID), alpha = 0.1) +
+  facet_wrap(~ metric,scales = "free_y")
